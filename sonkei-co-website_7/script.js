58c26b62f2b37642b8b88e694557680a9a31b736
LiliG1985 <liligreen85@gmail.com>
2026-09-02 21:24:44 +0400

    Delete sonkei-co-website_7 directory

--- sonkei-co-website_7/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var burger = document.querySelector('.nav-burger');
-  var menu = document.querySelector('.mobile-menu');
-  if (burger && menu) {
-    burger.addEventListener('click', function () {
-      menu.classList.toggle('open');
-    });
-  }
-});
